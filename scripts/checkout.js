01043f02decdbc04b84d48e1a81c892ceca87203
blah <blah>
2023-08-02 21:35:14 -0400

    delete products from html when hit 'delete'

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -11,7 +11,8 @@ cart.forEach ((cartItem) => {
 
   cartSummaryHTML +=
   `
-  <div class="cart-item-container">
+  <div class="cart-item-container
+    js-cart-item-container-${productId}">
     <div class="delivery-date">
       Delivery date: Wednesday, June 15
     </div>
@@ -96,5 +97,10 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click' , () => {
       const productId=link.dataset.productId;
       removeFromCart ( productId );
+
+      const container= document.querySelector(
+        `.js-cart-item-container-${productId}`
+        );
+        container.remove();
     });
   });
