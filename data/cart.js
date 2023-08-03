8468a2722589e122ecb4df6908abc8443159f70f
blah <blah>
2023-08-02 21:47:58 -0400

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,11 @@
-export let cart = [{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 2,
-},
-{ productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1,
-}]; //default/test values
+export let cart = JSON.parse ( localStorage.getItem (  'cart' ) ) || 
+  [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  { productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+  }];
 
 
 //-------------------------------------------------------------------------------
@@ -17,6 +18,8 @@ export function addToCart(productId){
       quantity: 1
     }) //cart.push
   } else cart[index].quantity++;
+
+  saveToStorage();
 }//addToCart
 
 
@@ -25,4 +28,13 @@ export function addToCart(productId){
 export function removeFromCart(productId){
   const index=cart.findIndex (product => product.productId===productId);
   cart.splice(index,1);
+
+  saveToStorage();
+}
+
+
+
+//-------------------------------------------------------------------------------
+function saveToStorage(){
+  localStorage.setItem ( 'cart' , JSON.stringify ( cart ) ) ;
 }
