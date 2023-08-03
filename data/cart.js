b807f231cddf5c3d4e60c06a0d37921dfbddc622
blah <blah>
2023-08-02 21:25:49 -0400

    delete products from cart when click 'delete'

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2,
 },
@@ -18,3 +18,11 @@ export function addToCart(productId){
     }) //cart.push
   } else cart[index].quantity++;
 }//addToCart
+
+
+
+//-------------------------------------------------------------------------------
+export function removeFromCart(productId){
+  const index=cart.findIndex (product => product.productId===productId);
+  cart.splice(index,1);
+}
